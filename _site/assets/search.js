2447577ae0521cae085b4c020accacd6611f3a10
Matteo Vilucchio <matteovilucchio@Matteos-MBP.homenet.telecomitalia.it>
2020-06-14 23:37:07 +0200

    added reading time code

--- _site/assets/search.js
+++ _site/assets/search.js
@@ -1,1 +1,1 @@
-window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Test No.1",'url':"/2019/01/01/test.html"}]};
+window.TEXT_SEARCH_DATA={'posts':[{'title':"Post with Header Image",'url':"/2018/06/01/header-image.html"},{'title':"Welcome",'url':"/2018/07/01/welcome.html"},{'title':"Test No.1",'url':"/2019/01/01/test.html"},{'title':"Asymmetrical Anharmonic Oscillator",'url':"/2020/06/25/asymmetric-anarmonic-hoscillator.html"}]};
